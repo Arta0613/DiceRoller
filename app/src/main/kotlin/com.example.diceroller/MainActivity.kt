f5f7a76098846cddb15b3a278f2d3d408842e5c4
Mohammed Amin <arta0613@gmail.com>
2019-11-23 22:24:57 -0500

    add roll button functionality

--- app/src/main/kotlin/com.example.diceroller/MainActivity.kt
+++ app/src/main/kotlin/com.example.diceroller/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupRollButton()
+    }
+
+    private fun setupRollButton() {
+        roll_button.text = getString(R.string.roll)
+        roll_button.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        roll_result.text = (Random.nextInt(6) + 1).toString()
     }
 }
